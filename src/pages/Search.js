f3419d660fc4467bc40fbb0878cf40d3dc19972d
namph-uet <hoangnamuet.vnu@gmail.com>
2021-09-20 23:18:32 +0700

    feat: set up

---
+++ src/pages/Search.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import SearchIcon from "@material-ui/icons/Search";
+import MicIcon from "@material-ui/icons/Mic";
+import "./Search.css"
+import { Button } from "@material-ui/core"
+
+function Search() {
+    const [input, setInput] = useState("");
+
+    const search = e => {
+        e.preventDefault();
+    }
+    return (
+        <div className="search">
+            <div className="search_input">
+                <SearchIcon className="search_inputIcon"/>
+                <input value={input} onChange={ e => setInput(e.target.value)}/>
+                <MicIcon />
+            </div>
+
+            <div className="search_buttons">
+                <Button type="submit" variant="outlined" onClick={search}>Google Search</Button>
+                <Button variant="outlined">I'm feeling Luckey</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Search
